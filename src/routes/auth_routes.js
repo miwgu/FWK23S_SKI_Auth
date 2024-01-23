59876ef3fe01d0c55844d4949f3f3b54cb56c5b7
ellinor Jansson lande <ellinor_jansson@hotmail.com>
2024-01-23 10:02:47 +0100

    lagt till kommentarer i koden så man kan läsa vad det innebär

--- src/routes/auth_routes.js
+++ src/routes/auth_routes.js
@@ -1,25 +1,32 @@
 const express = require('express');
+//importerar jwt
 const jwt = require('jsonwebtoken');
+//importerar crypto
 const crypto = require('crypto');
-
+//skapar en router
 const router = express.Router();
+//hämtar secret key från .env filen
 const secretKey = process.env.SECRET_KEY;
-
+//skriver ut secret key
 console.log(`SecretKey: ${secretKey}`);
+//skriver ut en random hex sträng
+app.use(crypto.randomBytes(64).toString('hex'));
 
 // Fördefinierade användare
 const users = [
   { email: "user@example.com", password: "password" },
-  { email: "miwa@example.com", password: "password" },
+  { email: "other@example.com", password: "password" },
   { email: "admin@example.com", password: "password" },
 ];
 
 /**
- * Kontrollerar om användaren är giltig.
+ * parameter email används för att jämföra med användarens e-post och parameter password används för att jämföra med användarens lösenord.
  * @param {string} email Användarens e-post.
  * @param {string} password Användarens lösenord.
  * @returns {boolean} Sant om användaren är giltig, annars falskt.
  */
+
+//kollar om användaren är giltig genom att jämföra e-post och lösenord
 function isValidUser(email, password) {
   return users.some(user => user.email === email && user.password === password);
 }
@@ -33,21 +40,23 @@ function getUserRole(email) {
   return email === 'admin@example.com' ? 'admin' : 'user';
 }
 
-// Login-route
+// Login route med JWT som skickas tillbaka som JSON
 router.post('/login', (req, res) => {
   const { email, password } = req.body;
 
+  // Kontrollera om användaren är giltig och sätt en gräns för giltighetstiden på 1 timme
   if (isValidUser(email, password)) {
     const payload = {
       email,
       role: getUserRole(email),
       exp: Math.floor(Date.now() / 1000) + (60 * 60) // 1 timme (3600 sekunder)
     };
-
+  
+  // Skapa och signera JWT med vår hemliga nyckel och skicka tillbaka den som JSON
     const token = jwt.sign(payload, secretKey);
     console.log(`Generated JWT: ${token}`);
-    console.log('You are now logged in!');
-
+    console.log('WELCOME! You are now logged in!');
+  // om användaren inte är giltig skickas ett felmeddelande tillbaka
     res.json({ token });
   } else {
     console.log('Invalid email or password');
@@ -55,4 +64,6 @@ router.post('/login', (req, res) => {
   }
 });
 
+// Exportera modulen för att kunna användas av andra moduler (t.ex. server.js) via require() funktionen 
 module.exports = router;
+
