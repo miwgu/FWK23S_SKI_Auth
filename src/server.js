48e4270d746625934f468878cd05e5cb71ff44f3
ellinor Jansson lande <ellinor_jansson@hotmail.com>
2024-01-25 13:52:32 +0100

    lagt till mer kommentarer i övrig kod

--- src/server.js
+++ src/server.js
@@ -1,13 +1,20 @@
+//installerar express för att kunna använda express.Router()
 const express = require('express');
+//body parser är en middleware som används för att kunna tolka JSON
 const bodyParser = require('body-parser');
+//importerar routes från auth_routes.js
 const authRoutes = require('./routes/auth_routes');
+//cors används för att kunna göra cross-origin requests på ett säkert sätt
 const cors = require('cors'); 
+//helmet används för att sätta säkerhetsheaders och skydda appen från vissa webbattacker
 const helmet = require('helmet');
 
 const app = express();
+//använder helmet i appen för att sätta säkerhetsheaders och skydda appen från vissa webbattacker
 app.use(helmet());
 
-app.use(cors());  // Use CORS middleware without any restrictions
+//använder cors i appen för att kunna göra cross-origin requests på ett säkert sätt
+app.use(cors());
 app.use(bodyParser.json());
 app.use('/auth', authRoutes);
 
